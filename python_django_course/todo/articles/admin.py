998f38f04638648a8391154508a0d19cd3546640
khotanscoder <khotansdr@gmail.com>
2021-07-26 18:10:30 +0430

    --authurization--

--- python_django_course/todo/articles/admin.py
+++ python_django_course/todo/articles/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Article
+from .models import Article, Comment
 
-admin.site.register(Article)
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
